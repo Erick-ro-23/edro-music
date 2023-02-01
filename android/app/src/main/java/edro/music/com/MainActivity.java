74d3a39ba499e2dc6c45430ca2e2b7e95d7d7eea
Erick-ro-23 <76449897+Erick-ro-23@users.noreply.github.com>
2023-02-01 17:19:28 +0100

    Slides y paginas de inicio bien v1

---
+++ android/app/src/main/java/edro/music/com/MainActivity.java
@@ -0,0 +1,5 @@
+package edro.music.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'edro.music.com',
   appName: 'edro-music',
   webDir: 'www',
   bundledWebRuntime: false
